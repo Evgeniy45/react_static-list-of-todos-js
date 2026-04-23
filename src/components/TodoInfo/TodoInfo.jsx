a0525eb2fcf22b749642631d994600d523af15c0
Evgeniy45 <jekaolhovskii@gmail.com>
2026-04-23 22:46:37 +0300

    finished task react_static-list-of-todos

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,2 +1,15 @@
-// Add the required props
-export const TodoInfo = () => <>TodoInfo markup</>;
+import clsx from 'clsx';
+import './TodoInfo.scss';
+import { UserInfo } from '../UserInfo';
+
+export const TodoInfo = ({ todo }) => {
+  return (
+    <article
+      className={clsx('TodoInfo', { 'TodoInfo--completed': todo.completed })}
+    >
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+
+      <UserInfo user={todo.user} />
+    </article>
+  );
+};
