bd83c5e0fb51e211c6191e90141d93a9a3588930
Evgeniy45 <jekaolhovskii@gmail.com>
2026-04-23 23:01:23 +0300

    removed clsx

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,11 +1,10 @@
-import clsx from 'clsx';
 import './TodoInfo.scss';
 import { UserInfo } from '../UserInfo';
 
 export const TodoInfo = ({ todo }) => {
   return (
     <article
-      className={clsx('TodoInfo', { 'TodoInfo--completed': todo.completed })}
+      className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}
     >
       <h2 className="TodoInfo__title">{todo.title}</h2>
 
